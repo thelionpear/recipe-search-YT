cc3377d34fbe3077211a98eddfdb1a180195c727
Michelle Garcia <thelionpear@gmail.com>
2018-11-09 16:20:54 -0700

    API set up

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 
 import Form from './components/Form'
 
+const API_KEY = `${process.env.REACT_APP_API_KEY}`;
+
 class App extends Component {
+
+  getRecipe = (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault(); 
+    console.log(recipeName);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
-        <Form/> 
+        <Form getRecipe={this.getRecipe}/> 
       </div>
     );
   };
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,8 @@
 import React from 'react'; 
 
-const Form = () => (
-  <form>
-    <input type="text"/> 
+const Form = props => (
+  <form onSubmit={props.getRecipe}>
+    <input type="text" name="recipeName" /> 
     <button>Search</button> 
   </form> 
 );
